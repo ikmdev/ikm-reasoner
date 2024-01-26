138388975cfb1b71aa45237ce64feab93f07a51f
jenkins <118758835+rbradleyD@users.noreply.github.com>
2024-01-26 20:33:42 +0000

    Release 0.14.0

--- elk-owlapi-parent/elk-owlapi4-tests/src/test/java/org/semanticweb/elk/owlapi/proofs/ProofTestUtils.java
+++ elk-owlapi-parent/elk-owlapi4-tests/src/test/java/org/semanticweb/elk/owlapi/proofs/ProofTestUtils.java
@@ -1,10 +1,25 @@
 /*
+ * Copyright © 2023 Integrated Knowledge Management (support@ikm.dev)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.semanticweb.elk.owlapi.proofs;
+
+/*-
  * #%L
- * ELK Proofs Package
- * $Id:$
- * $HeadURL:$
+ * ELK OWL API v.4 Binding for Test
  * %%
- * Copyright (C) 2011 - 2014 Department of Computer Science, University of Oxford
+ * Copyright (C) 2023 - 2024 Integrated Knowledge Management
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -19,7 +34,6 @@
  * limitations under the License.
  * #L%
  */
-package org.semanticweb.elk.owlapi.proofs;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
