824bab368f8df2224fca0a328e207a45cc60c6a4
Keith E Campbell <3868866+kec@users.noreply.github.com>
2025-11-26 13:09:42 -0800

    chore: exclude caffeine dependency and improve logging format
    - Exclude com.github.ben-manes.caffeine from all OWLAPI and ELK dependencies in elk-owlapi module
    - Format counter output with thousand separators in NecessaryNormalFormBuilder logging

--- elk-snomed/src/main/java/dev/ikm/elk/snomed/NecessaryNormalFormBuilder.java
+++ elk-snomed/src/main/java/dev/ikm/elk/snomed/NecessaryNormalFormBuilder.java
@@ -166,14 +166,14 @@ public class NecessaryNormalFormBuilder {
 		int cnt = 0;
 		for (Concept concept : concepts) {
 			if (++cnt % 50000 == 0)
-				LOG.info("Generate: " + cnt);
+				LOG.info("Generate: " + String.format("%,d", cnt));
 			Definition def = generateNNF(concept, false);
 			Concept nnf_con = new Concept(concept.getId());
 			nnf_con.addDefinition(def);
 			if (concept_comparer != null)
 				concept_comparer.compare(nnf_con);
 		}
-		LOG.info("Generate: " + cnt);
+		LOG.info("Generate: " + String.format("%,d", cnt));
 		if (concept_comparer != null && concept_comparer.getMisMatchCount() != 0)
 			LOG.error("Mis-match: " + concept_comparer.getMisMatchCount());
 	}
